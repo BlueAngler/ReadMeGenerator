ca93340895c3bd95fb77542bfa76922337eea308
BlueAngler <dannyo1028@gmail.com>
2022-09-19 21:06:02 -0500

    added prompts

---
+++ README.md
@@ -0,0 +1,8 @@
+C:\Users\80060250\Desktop\MSU\Challenges\Challenge-9\ReadMeGenerator\index.js
+
+
+
+Screencastify
+https://drive.google.com/file/d/1Bg7kZaeXLgbsqRL3T75E2Utkugt7Jg4p/view
+
+
--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Project Title?',
+    },
+    {
+      type: 'input',
+      message: 'Description',
+      name: 'ReadmeDescription',
+      
+    },
+    {
+      type: 'input',
+      message: 'Install instructions?',
+      name: 'Installation',
+      
+    },
+    {
+        type: 'input',
+        message: 'Usage Information',
+        name: 'READMEDESCRIPTION',
+        
+      },
+      {
+        type: 'input',
+        message: 'Contribution Guidelines',
+        name: 'Guidelines',
+        
+      },
+      {
+        type: 'input',
+        message: 'Test Instructions',
+        name: 'Instructions',
+        
+      },
+  ])
+
+  
+
 
 fs.writeFile('README.md', process.argv[1], (err) =>
 err ? console.error(err) : console.log('Successfully create a README file!')
 ); 
 
 
+ 
